75569957ef17c3030d610d088f73f25130f3318b
Andrew Brobston <andrew@brobston.com>
2014-11-11 21:32:38 -0600

    Added copyright headers.

--- src/main/java/com/brobston/indelible/definition/ClassDefinition.java
+++ src/main/java/com/brobston/indelible/definition/ClassDefinition.java
@@ -1,5 +1,20 @@
 package com.brobston.indelible.definition;
 
+/*
+ * Indelible: to counteract type erasure.
+ * Copyright © 2014, Brobston Development, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of either the GNU General Public
+ * License, version 2, or the three-clause BSD License, at your
+ * option.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License or the BSD License for more details.
+ */
+
 /**
  * Created by andrew on 11/1/14.
  */
